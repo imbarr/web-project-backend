e2ccd694f639b51546783840fbf7dc7731fa3cd1
imbar <imbrkvskh@gmail.com>
2019-01-20 21:53:51 +0500

    explicit column names in queries

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -15,13 +15,15 @@ class Database(config: DatabaseConfig)(implicit executionContext: ExecutionConte
     config.password)
 
   def addPayment(payment: Payment): IO[Int] = {
-    sql"""insert into payments values (${payment.cardNumber}, ${payment.expirationMonth}, ${payment.expirationYear},
-         |${payment.CVC}, ${payment.money.toInt}, ${payment.comment}, ${payment.email}, 1);"""
+    sql"""insert into payments (card_number, expiration_month, expiration_year, cvc, money, comment, email)
+         |values (${payment.cardNumber}, ${payment.expirationMonth}, ${payment.expirationYear},
+         |${payment.CVC}, ${payment.money.toInt}, ${payment.comment}, ${payment.email});"""
       .stripMargin.update.run.transact(xa)
   }
 
   def addRequest(request: Request): IO[Int] = {
-    sql"""insert into requests values (${request.taxId}, ${request.BIC}, ${request.accountNumber},
+    sql"""insert into requests (tax_id, bic, account_number, vat, money, telephone, email)
+         |values (${request.taxId}, ${request.BIC}, ${request.accountNumber},
          |${request.VAT.toString}, ${request.money.toInt}, ${request.telephone}, ${request.email});"""
       .stripMargin.update.run.transact(xa)
   }
