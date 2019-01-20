16667d24871693033b393d5e4e9bf8cd7cd108b9
imbar <imbrkvskh@gmail.com>
2019-01-20 20:52:49 +0500

    fixed constraints

--- src/main/scala/domain/Payment.scala
+++ src/main/scala/domain/Payment.scala
@@ -8,14 +8,14 @@ case class Payment(cardNumber: String,
                    expirationDate: String,
                    CVC: String,
                    money: String,
-                   comment: Option[String],
+                   comment: String,
                    email: String) {
   require("[0-9]{16}".r matches cardNumber)
   require("([0-9]|0[0-9]|1[0-2])\\/[1-9][0-9]".r matches expirationDate)
-  require("[0-9]{3}" matches CVC)
+  require("[0-9]{3}".r matches CVC)
   require(Try(money.toInt).map(m => m >= 1000 && m <= 75000).getOrElse(false))
-  require(comment.forall(_.length <= 150))
-  require("(?=[^\\s]+)(?=(\\w+)@([\\w\\.]+))".r matches email)
+  require(comment.length <= 150)
+  require("[^@]+@[^\\.]+\\..+".r matches email)
 
   val expirationMonth: Int = expirationDate.split("/")(0).toInt
   val expirationYear: Int = expirationDate.split("/")(1).toInt
