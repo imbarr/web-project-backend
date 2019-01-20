adeee9a9acc4a697c42bb33ee672c92a9d94bb2d
imbar <imbrkvskh@gmail.com>
2019-01-20 14:28:38 +0500

    added missing fields

--- src/main/scala/domain/InternetBankPayment.scala
+++ src/main/scala/domain/InternetBankPayment.scala
@@ -3,7 +3,11 @@ package domain
 import domain.VATOptions.VATOptions
 import util.RichRegex.RichRegex
 
-case class InternetBankPayment(taxId: String, BIC: String, VAT: VATOptions) {
+import scala.util.Try
+
+case class InternetBankPayment(taxId: String, BIC: String, accountNumber: String, VAT: VATOptions, money: String) {
   require("([0-9]{10}|[0-9]{12})".r matches taxId)
   require("[0-9]{9}".r matches BIC)
+  require("[0-9]{20}".r matches accountNumber)
+  require(Try(money.toInt).map(m => m >= 1000 && m <= 75000).getOrElse(false))
 }
--- src/main/scala/domain/Request.scala
+++ src/main/scala/domain/Request.scala
@@ -1,8 +1,16 @@
 package domain
 
+import domain.VATOptions.VATOptions
 import util.RichRegex.RichRegex
 
-class Request(accountNumber: String, telephone: String) {
+import scala.util.Try
+
+class Request(taxId: String, BIC: String, accountNumber: String,
+              VAT: VATOptions, money: String, telephone: String, email: String) {
+  require("([0-9]{10}|[0-9]{12})".r matches taxId)
+  require("[0-9]{9}".r matches BIC)
   require("[0-9]{20}".r matches accountNumber)
+  require(Try(money.toInt).map(m => m >= 1000 && m <= 75000).getOrElse(false))
   require("+7[0-9]{10}".r matches telephone)
+  require("(?=[^\\s]+)(?=(\\w+)@([\\w\\.]+))".r matches email)
 }
