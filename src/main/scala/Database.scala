afc2f9eaa95f583439a10d73bd0b2582e4673963
imbar <imbrkvskh@gmail.com>
2019-01-21 22:45:43 +0500

    admin panel queries

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -16,13 +16,13 @@ class Database(config: DatabaseConfig)(implicit executionContext: ExecutionConte
     config.password)
 
   def addPayment(payment: Payment): IO[Unit] = updateSingleValue(
-    sql"""insert into payments (card_number, expiration_month, expiration_year, cvc, money, comment, email)
+    fr"""insert into payments (card_number, expiration_month, expiration_year, cvc, money, comment, email)
          |values (${payment.cardNumber}, ${payment.expirationMonth}, ${payment.expirationYear},
          |${payment.CVC}, ${payment.money.toInt}, ${payment.comment}, ${payment.email});"""
   )
 
   def addRequest(request: Request): IO[Unit] = updateSingleValue(
-    sql"""insert into requests (tax_id, bic, account_number, vat, money, telephone, email)
+    fr"""insert into requests (tax_id, bic, account_number, vat, money, telephone, email)
          |values (${request.taxId}, ${request.BIC}, ${request.accountNumber},
          |${request.VAT.toString}, ${request.money.toInt}, ${request.telephone}, ${request.email});"""
   )
@@ -36,20 +36,35 @@ class Database(config: DatabaseConfig)(implicit executionContext: ExecutionConte
   }
 
   def getPaymentRange(rangeRequest: RangeRequest): IO[List[DBPayment]] = {
-    getRange("payments", rangeRequest).query[DBPayment].to[List].transact(xa)
+    if(!List("id", "cardNumber", "expirationMonth", "expirationYear", "CVC", "money",
+      "comment", "email", "isSafe").contains(rangeRequest.sortField))
+      throw new Exception("Table payments does not have column " + rangeRequest.sortField)
+
+    (fr"""select * from payments where id between ${rangeRequest.start} and ${rangeRequest.end} order by""" ++
+      Fragment.const(rangeRequest.sortField) ++ Fragment.const(rangeRequest.order.toString))
+      .stripMargin.query[DBPayment].to[List].transact(xa)
+  }
+
+  def getPaymentRowNumber: IO[Int] = {
+    fr"select count(*) from payments".query[Int].unique.transact(xa)
+  }
+
+  def getRequestRowNumber: IO[Int] = {
+    fr"select count(*) from requests".query[Int].unique.transact(xa)
   }
 
   def getRequestRange(rangeRequest: RangeRequest): IO[List[DBRequest]] = {
-    getRange("requests", rangeRequest).query[DBRequest].to[List].transact(xa)
-  }
+    if(!List("id", "taxId", "BIC", "accountNumber", "VAT", "money", "telephone", "email")
+      .contains(rangeRequest.sortField))
+      throw new Exception("Table requests does not have column " + rangeRequest.sortField)
 
-  def getRange(table: String, rangeRequest: RangeRequest): Fragment = {
-    sql"""select * from $table where id between ${rangeRequest.start} and ${rangeRequest.end}
-         |order by ${rangeRequest.sortField} ${rangeRequest.order.toString};"""
+    (fr"""select * from requests where id between ${rangeRequest.start} and ${rangeRequest.end} order by""" ++
+      Fragment.const(rangeRequest.sortField) ++ Fragment.const(rangeRequest.order.toString))
+      .stripMargin.query[DBRequest].to[List].transact(xa)
   }
 
   def changeSafety(safetyRequest: SetSafetyRequest): IO[Unit] = {
     val safe = if(safetyRequest.isSafe) 1 else 0
-    updateSingleValue(sql"""update payments set isSafe = $safe where id = ${safetyRequest.id}""")
+    updateSingleValue(fr"""update payments set isSafe = $safe where id = ${safetyRequest.id}""")
   }
 }
