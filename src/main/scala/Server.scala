05ecc4009e4b97d625787974786bd32f140e15e4
imbar <imbrkvskh@gmail.com>
2019-01-21 17:31:18 +0500

    added routes for admin panel, some refactoring

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -7,6 +7,7 @@ import akka.stream.ActorMaterializer
 import cats.effect.IO
 import com.microsoft.sqlserver.jdbc.SQLServerException
 import com.typesafe.scalalogging.Logger
+import domain.SortingOptions.SortingOptions
 import domain._
 import domain.json.Decoding._
 import io.circe.Json
@@ -32,12 +33,8 @@ class Server(config: Config)
   val failure = complete(HttpResponse(StatusCodes.InternalServerError, CORSHeaders))
   val badRequest = complete(HttpResponse(StatusCodes.BadRequest, CORSHeaders))
 
-  def insertToRoute[T](result: IO[Int]) =
-    onComplete(result.flatMap(
-      i =>
-        if(i == 1) IO(Unit)
-        else IO.raiseError(new Exception("No data modified"))
-    ).unsafeToFuture()) {
+  def IOToRoute[T](monad: IO[T]) =
+    onComplete(monad.unsafeToFuture()) {
       case Success(_) =>
         log.debug("Data inserted")
         success
@@ -46,18 +43,39 @@ class Server(config: Config)
         failure
     }
 
-  val route = post {
-    path("credit-card") {
-      entity(as[Payment]) { payment =>
-        insertToRoute(db.addPayment(payment))
+  val route =
+    post {
+      path("credit-card") {
+        entity(as[Payment]) { payment =>
+          IOToRoute(db.addPayment(payment))
+        }
+      } ~
+      path("internet-bank") {
+        failure
+      } ~
+      path("ask") {
+        entity(as[Request]) { request =>
+          IOToRoute(db.addRequest(request))
+        }
       }
     } ~
-    path("internet-bank") {
-      failure
+    get {
+      path("payment") {
+        entity(as[RangeRequest]) { param =>
+          IOToRoute(db.getPaymentRange(param))
+        }
+      } ~
+      path("request") {
+        entity(as[RangeRequest]) { param =>
+          IOToRoute(db.getRequestRange(param))
+        }
+      }
     } ~
-    path("ask") {
-      entity(as[Request]) { request =>
-        insertToRoute(db.addRequest(request))
+    put {
+      path("payment") {
+        entity(as[SetSafetyRequest]) { param =>
+          IOToRoute(db.changeSafety(param))
+        }
       }
     } ~
     entity(as[Json]) { _ =>
@@ -65,12 +83,11 @@ class Server(config: Config)
       badRequest
     } ~
     extractRequest { _ =>
-      log.info("Error: not a json request")
-//      request.entity.toStrict(FiniteDuration(1000, TimeUnit.MILLISECONDS))
-//        .map(_.data.utf8String).map(println)
+      log.info("Not a json request")
+  //      request.entity.toStrict(FiniteDuration(1000, TimeUnit.MILLISECONDS))
+  //        .map(_.data.utf8String).map(println)
       badRequest
     }
-  }
 
   def start(): Future[Http.ServerBinding] = {
     Http().bindAndHandle(route, config.server.interface, config.server.port)
