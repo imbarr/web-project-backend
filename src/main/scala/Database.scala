0bc004f3c4d81a11c87ed74d8b63039ba8dddaa3
imbar <imbrkvskh@gmail.com>
2019-01-21 17:30:56 +0500

    added queries for admin panel

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -3,6 +3,7 @@ import domain._
 import cats.effect.IO
 import doobie.implicits._
 import domain.Payment
+import doobie.util.fragment.Fragment
 
 import scala.concurrent.ExecutionContext
 
@@ -14,17 +15,43 @@ class Database(config: DatabaseConfig)(implicit executionContext: ExecutionConte
     config.user,
     config.password)
 
-  def addPayment(payment: Payment): IO[Int] = {
+  def addPayment(payment: Payment): IO[Unit] = updateSingleValue(
     sql"""insert into payments (card_number, expiration_month, expiration_year, cvc, money, comment, email)
          |values (${payment.cardNumber}, ${payment.expirationMonth}, ${payment.expirationYear},
          |${payment.CVC}, ${payment.money.toInt}, ${payment.comment}, ${payment.email});"""
-      .stripMargin.update.run.transact(xa)
-  }
+  )
 
-  def addRequest(request: Request): IO[Int] = {
+  def addRequest(request: Request): IO[Unit] = updateSingleValue(
     sql"""insert into requests (tax_id, bic, account_number, vat, money, telephone, email)
          |values (${request.taxId}, ${request.BIC}, ${request.accountNumber},
          |${request.VAT.toString}, ${request.money.toInt}, ${request.telephone}, ${request.email});"""
-      .stripMargin.update.run.transact(xa)
+  )
+
+  def updateSingleValue(query: Fragment): IO[Unit] = {
+    query.stripMargin.update.run.transact(xa).map(
+      i =>
+        if(i == 1) Unit
+        else throw new Exception("No data modified")
+    )
+  }
+
+  def getPaymentRange(rangeRequest: RangeRequest): IO[List[DBPayment]] = {
+    getRange("payments", rangeRequest).query[DBPayment].to[List].transact(xa)
+  }
+
+  def getRequestRange(rangeRequest: RangeRequest): IO[List[DBRequest]] = {
+    getRange("requests", rangeRequest).query[DBRequest].to[List].transact(xa)
+  }
+
+  def getRange(table: String, rangeRequest: RangeRequest): Fragment = {
+    sql"""select * from $table where id between ${rangeRequest.start} and ${rangeRequest.end}
+         |order by ${rangeRequest.sortField} ${rangeRequest.order.toString};"""
+  }
+
+  def changeSafety(safetyRequest: SetSafetyRequest): IO[Unit] = {
+    val safe = if(safetyRequest.isSafe) 1 else 0
+    updateSingleValue(sql"""update payments set isSafe = $safe where id = ${safetyRequest.id}""")
   }
 }
+
+import akka.http.scaladsl.server.
